8b2bf10cf215df3b39550d4c2d18b5da81cae5c8
Raj-karan-singh <rajkaran.singh194@gmail.com>
2026-02-12 12:51:17 +0530

    Add files via upload

---
+++ linkedlist.py
@@ -0,0 +1,10 @@
+class Linkedlist:
+    def __init__(self,dat):
+        new = Linkedlist(x)
+        if self.head is None:
+            self.head=None
+            
+        else:
+            new.link = self.head
+            self.head = new
+    
